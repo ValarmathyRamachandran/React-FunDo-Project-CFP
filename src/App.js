09ddff6deaadf65315430ad7f79f1b722ea566dc
Valarmathy <valarmathy.ramachandran@outlook.com>
2022-01-08 00:55:13 +0530

    [Valarmathy]: Created SignUp page for create Google Account

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import SignIn from './pages/SignIn/SignIn';
+import SignUp from './pages/SignUp/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <SignIn />
+      <SignUp />
     </div>
   );
 }
